9e50e835f357efbc7444776d43633310b7efd696
ALI MOHAMAN <alimohaman00@gmail.com>
2024-08-21 21:48:27 +0100

    correction de l'orthographe de la table utilisateur

--- src/main/java/com/almo/noteApp/entity/Utilisateur.java
+++ src/main/java/com/almo/noteApp/entity/Utilisateur.java
@@ -15,7 +15,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity(name = "Utilisateur")
-@Table(name = "utilisateur")
+@Table(name = "utilisateurs")
 @Builder
 public class Utilisateur {
     @Id
